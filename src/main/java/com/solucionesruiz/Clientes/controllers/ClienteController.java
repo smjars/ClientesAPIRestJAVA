2df2a30b3294f8fbc3d081a32fb15b1e0c49970f
smjar <smjars@hotmail.com>
2024-12-13 14:55:32 +0100

    Se agrego la clase getcliente, que recibe un username por parametro y si lo encuentra en la lista de clientes lo devuelve, ejem localhosto:8080/clientes/KKroto

--- src/main/java/com/solucionesruiz/Clientes/controllers/ClienteController.java
+++ src/main/java/com/solucionesruiz/Clientes/controllers/ClienteController.java
@@ -2,6 +2,7 @@ package com.solucionesruiz.Clientes.controllers;
 
 import com.solucionesruiz.Clientes.domain.Cliente;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.ArrayList;
@@ -10,7 +11,7 @@ import java.util.List;
 @RestController
 public class ClienteController {
     // creo una lista de clientes ficticia (deberia estar en la base de datos y extraido en otro paquete)
-    private List<Cliente> clientes = new ArrayList<>(Arrays.asList(
+    private final List<Cliente> clientes = new ArrayList<>(Arrays.asList(
             new Cliente(111, "Alfredo", "ALF", "Estoy vivo!!"),
             new Cliente(112, "Angel", "ANG", "Quiero volar"),
             new Cliente(113, "Goku", "KKorto", "Kamehamehaaaaa"),
@@ -23,5 +24,15 @@ public class ClienteController {
     public List<Cliente> getClientes()
     {
         return clientes;
+    }
+    // retorno el cliente si el paremetro username existe en la lista, en caso de no encontrarlo devuelvo null(esto es por mientras)
+    @GetMapping("clientes/{username}")
+    public Cliente getCliente(@PathVariable String username){
+        for (Cliente c : clientes){
+            if (c.getUsername().equalsIgnoreCase(username)){
+                return c;
+            }
+        }
+        return null;
     }
 }
